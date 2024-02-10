b2ec44a8522592738144fd355125293075d754bb
Obi1knube <123329625+Obi1knube@users.noreply.github.com>
2024-02-10 21:48:38 +0000

    Delete button works

--- public/js/profile.js
+++ public/js/profile.js
@@ -46,15 +46,5 @@ document
 
 // Attach event listener to the blog list for delete button clicks
 document
-  .querySelector('.blog-list')
+  .querySelector('.post-list')
   .addEventListener('click', delButtonHandler);
-
-//   In this modified code, the newFormHandler function handles the creation of a new blog.
-//   It retrieves the title and content values from the form, sends a POST request to the /api/blogs endpoint,
-//   and redirects to the profile page if the request is successful.
-
-// The delButtonHandler function handles the deletion of a blog. It checks if the clicked element has a data-id attribute,
-//  retrieves the blog ID from the attribute, sends a DELETE request to the /api/blogs/:id endpoint,
-//  and redirects to the profile page if the request is successful.
-
-// Make sure to update your HTML file to use the correct element IDs and class names for the form and list elements.
